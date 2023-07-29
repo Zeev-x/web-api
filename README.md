37fa7c7206b86253b9b1f1c79520c94e8254d78a
Zeevalya <87901027+Zeev-x@users.noreply.github.com>
2023-07-29 10:58:00 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# web-api
+web  api nodejs
+
+## Lihat hasil
+[Click here](https://zeevalya.cyclic.app/)
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = process.env.PORT || 8080;
+
+const conf = express.static(path.resolve("./public"));
+app.use(conf);
+app.use(express.json());
+app.set("json spaces",1);
+
+app.listen(port,() => {
+  console.log("Website berjalan di http://localhost:"+port);
+});
+
+app.get("/", async(req,res) => {
+  fs.readFile("./public/index.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/home", async(req,res) => {
+  fs.readFile("./public/home.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/home/bocchi", async(req,res) => {
+  fs.readFile("./public/bocchi.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/home/ayaka", async(req,res) => {
+  fs.readFile("./public/ayaka.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/home/bunny", async(req,res) => {
+  fs.readFile("./public/bunny.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
+
+app.get("/home/osakana", async(req,res) => {
+  fs.readFile("./public/osakana.html",(err,data) => {
+    if(err) return res.json({mess : 'error code 404'});
+    res.writeHead(200,{"Content-Type" : "text/html"});
+    res.write(data);
+    res.end();
+  });
+});
